12e36e37f01bdd98f971b9bf1a9f1b0b0471cbb0
RyuuXiao <164801435+RyuuXiaoo@users.noreply.github.com>
2025-08-31 19:10:54 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ const atlanticApi = axios.create({
 app.get('/api/layanan', async (req, res) => {
     try {
         console.log('[LOG] Meminta daftar layanan dari VPedia...');
-        const response = await atlanticApi.get('/layanan/price-list');
+        const response = await atlanticApi.get('/layanan/price_list');
 
         if (response.data && response.data.success) {
             const layanan = response.data.data.map(item => {
